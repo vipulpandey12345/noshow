8e2ad3d02429c4a59eaccb4da7825e81fe4718fc
Vipul Pandey <54221581+vipulpandey12345@users.noreply.github.com>
2025-03-17 01:33:12 -0700

    added csv file/dataset, work in progress

--- noshow/backend/model.py
+++ noshow/backend/model.py
@@ -1,6 +1,17 @@
+# Data Processing
+import pandas as pd
+import numpy as np
 
+# Modelling
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score, confusion_matrix, precision_score, recall_score, ConfusionMatrixDisplay
+from sklearn.model_selection import RandomizedSearchCV, train_test_split
+from scipy.stats import randint
 
-
+# Tree Visualisation
+from sklearn.tree import export_graphviz
+from IPython.display import Image
+import graphviz
 
 """
 - random forest model with labels
